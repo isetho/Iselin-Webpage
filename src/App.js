7743784a4caca5703441a03ccc51ab8968f9f101
Iselin Thorsen Nilsen <iselin.thorsen.nilsen@gmail.com>
2022-04-05 19:48:23 +0200

    Initial setup of webpage

--- src/App.js
+++ src/App.js
@@ -3,21 +3,35 @@ import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload. Nice
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="header-container">
+        <h1 className="header">Portefølje</h1>
+      </div>
+      <div className="container mx-auto">
+        <div className="flex">
+          <div className="image-container w-1/4 p-4 m-3">
+            <p className="text-center my-10">Image</p>
+          </div>
+          <div className="text-container w-3/4 p-4 m-3">
+            <p className="text-center my-10">Text</p>
+          </div>
+        </div>
+      </div>
+      <div className="container mx-auto">
+        <div className="info-container p-4 m-3">
+          <p className="text-center my-10">Info</p>
+        </div>
+      </div>
+      <div className="container mx-auto">
+        <div className="flex">
+          <div className="text-container w-3/4 p-4 m-3">
+            <p className="text-center my-10">Text</p>
+          </div>
+          <div className="image-container w-1/4 p-4 m-3">
+            <p className="text-center my-10">Image</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
