21bb59a3f1b79ebcd0f66a3ff1bc3d4032b2c1c8
Iselin Thorsen Nilsen <iselin.thorsen.nilsen@gmail.com>
2023-02-03 13:14:23 +0100

    Added intro component

---
+++ src/components/intro.js
@@ -0,0 +1,14 @@
+function Intro(props) {
+  return (
+    <div className="intro">
+      <p>
+        Iselin er en utvikler, designer og konsulent som bor i Bergen. Hun
+        engasjerer seg for universell utforming, gode brukeropplevelser og
+        grafisk design. Hun er utdannet innenfor Informasjonsteknologi ved
+        Høyskolen på Vestlandet og har en fartstid som utvikler på 1 år.
+      </p>
+    </div>
+  );
+}
+
+export default Intro;
